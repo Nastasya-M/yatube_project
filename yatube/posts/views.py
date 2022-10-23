192b612e8ede422127970acdb1f6f68635165fe0
Настасья Мартынова <martynovanastasya@gmail.com>
2022-10-23 19:11:08 +0300

    templates start

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request,template)
 
 def group_posts(request, slug):
     return HttpResponse('Группы постов {slug}')
